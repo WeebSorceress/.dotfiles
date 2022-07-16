ea991f53b2d9f919a571e37baeccfd83aac551b5
WeebSorceress <hello@weebsorceress.anonaddy.me>
2022-07-16 11:20:04 -0300

    feat: init digga template - removed darwin stuff - go only x86_64-linux - added personal cache - default stateVersion

---
+++ profiles/cachix/weeb-sorceress.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://weeb-sorceress.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "weeb-sorceress.cachix.org-1:p4PNpfq/O/CUoVEYz7bYFBYMkmcZO85CKYGu+u4E+Rc="
+    ];
+  };
+}
