7962ee1971356b165333f55a53592841ef4b98b8
WeebSorceress <hello@weebsorceress.anonaddy.me>
2022-07-17 15:26:17 -0300

    feat(users): add ssh key to root

--- users/root/default.nix
+++ users/root/default.nix
@@ -1,5 +1,7 @@
 { ... }:
-# recommend using `hashedPassword`
 {
-  users.users.root.password = "";
+  users.users.root = {
+    initialHashedPassword = "!";
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNUMD65dLiKkAGSDHaAVNxu1NJW8U23hQzUqHCymuk9 <ssh://siren|ed25519>" ];
+  };
 }
