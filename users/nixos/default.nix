bf195d051160aa49c7f07787a8f9cf2b53298e35
WeebSorceress <hello@weebsorceress.anonaddy.me>
2022-07-25 20:37:46 -0300

    feat: add ssh keys to to nixos user

--- users/nixos/default.nix
+++ users/nixos/default.nix
@@ -5,9 +5,13 @@
   enchantment.erase-sys-darlings.users = [ "nixos" ];
 
   users.users.nixos = {
-    initialPassword = "nixos";
-    description = "NixOS";
     isNormalUser = true;
+    description = "NixOS";
     extraGroups = [ "wheel" ];
+    initialPassword = "nixos";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNUMD65dLiKkAGSDHaAVNxu1NJW8U23hQzUqHCymuk9 <ssh://siren|ed25519>"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPXh4egJrGmJyFYY8r8GyjqM2OVo1lBZdoITd8S8TAc0 default-ssh@hercules-ci"
+    ];
   };
 }
