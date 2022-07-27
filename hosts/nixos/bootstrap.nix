bbeaf35e4b1a30f8286a7ec925f22ee21a65dabe
WeebSorceress <hello@weebsorceress.anonaddy.me>
2022-07-27 16:49:55 -0300

    fix: swap fileSystems entry to bootloader profile

--- hosts/nixos/bootstrap.nix
+++ hosts/nixos/bootstrap.nix
@@ -1,9 +1,4 @@
 { suites, profiles, ... }:
-# nixos-generate --format install-iso --flake '.#bootstrap'
-# OR
-# nix run github:nix-community/nixos-generators -- --format install-iso --flake '.#bootstrap'
 {
   imports = suites.main ++ [ profiles.bootloader.systemd-boot ];
-
-  fileSystems."/" = { device = "/dev/disk/by-label/nixos"; };
 }
--- profiles/bootloader/systemd-boot/default.nix
+++ profiles/bootloader/systemd-boot/default.nix
@@ -1,5 +1,7 @@
-{ ... }:
+{ lib, ... }:
 {
+  fileSystems."/" = lib.mkDefault { device = "/dev/disk/by-label/nixos"; };
+
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
